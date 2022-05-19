428edac669ea067e46b4a5506577453efb84e603
muddassir <102643522+muddassir19@users.noreply.github.com>
2022-05-19 11:16:30 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,7 +1,7 @@
 def call(tomcatIp,warName,credId){
     
      sshagent(["${credId}"]) {
-      sh "scp -o StrictHostKeyChecking=no traget/*.war ec2-user@${tomcatIp}:/opt/tomcat8/webapps/${warName}.war"
+      sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@${tomcatIp}:/opt/tomcat8/webapps/${warName}.war"
       sh 'ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/shutdown.sh'
       sh 'ssh ec2-user@${tomcatIp} /opt/tomcat8/bin/stratup.sh'
   }
